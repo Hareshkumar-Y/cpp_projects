1ee7b7496293f629336eb68150b292a95a197a63
Hareshkumar-Y <68243286+Hareshkumar-Y@users.noreply.github.com>
2024-09-12 10:34:00 +0530

    Question 5 completed

--- Qno4.cpp
+++ Qno4.cpp
@@ -1,4 +1,3 @@
-// Write a c++ program that accepts a user name and password as input and prints the same.
 #include<iostream>
 #include<string.h>
 using namespace std;
---
+++ Qno5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string name;
+    cin>>name;
+    cout<<"Hello "<<name<<"! Welcome to Examly Event Management System";
+    return 0;
+}
