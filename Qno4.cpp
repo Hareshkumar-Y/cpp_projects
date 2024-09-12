7359a8eb373b93cacc011884f16ed67c21faf863
Hareshkumar-Y <68243286+Hareshkumar-Y@users.noreply.github.com>
2024-09-12 10:24:44 +0530

    Question 4 completed

---
+++ Qno4.cpp
@@ -0,0 +1,11 @@
+// Write a c++ program that accepts a user name and password as input and prints the same.
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main() {
+    string username, password;
+    cin>>username>>password;
+    cout<<username<<'\t'<<password;
+    return 0;
+}
+    
