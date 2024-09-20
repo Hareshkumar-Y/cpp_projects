5e76dbd21d6344f539ed88c4bda914d4f3dc5d97
Hareshkumar-Y <68243286+Hareshkumar-Y@users.noreply.github.com>
2024-09-20 10:23:37 +0530

    Cpp conditionals created and question no 1 completed

---
+++ Cpp_conditionals/Qno1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age ;
+    char gen;
+    cin>>age>>gen;
+    gen=toupper(gen);
+    if(age<25 && gen == 'M')
+    {
+        cout<<"Group 1";
+    }
+    else if(age<25 && gen == 'F')
+    {
+        cout<<"Group 2";
+    }
+    else if (age>=25 && age<=45 && gen=='M')
+    {
+        cout<<"Group 3";
+    }
+    else if (age>=25 && age<=45 && gen=='F')
+    {
+        cout<<"Group 4";
+    }
+    else if (age>45 && (gen=='M' || gen=='F'))
+    {
+        cout<<"Group 5";
+    }
+    else
+    {
+        cout<<"Invalid input";
+    }
+    return 0;
+}        
