c1da986556db1bbc3a406cd77816eaf5138e68f2
Hareshkumar-Y <68243286+Hareshkumar-Y@users.noreply.github.com>
2024-09-20 12:19:09 +0530

    half baked completion

--- Cpp_looping/Qno2.cpp
+++ Cpp_looping/Qno2.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+    int num , factorial = 1;
+    cin>>num;
+    if (num<0)
+    {
+        cout<<"Error! Factorial of a negative number";
+    }
     
+    for(int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+    cout<<factorial;
     return 0;
 }
